c48dbb119e9f48d3a98ea30a620aa52a5f413d33
Parth Agarwal <agarwalparth616@gmail.com>
2026-09-14 15:31:09 +0530

    Checking seller and veryfication by admin

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -79,3 +79,50 @@ export const deleteProperty = async (req, res) => {
     });
   }
 };
+
+
+// GET PENDING SELLERS
+export const getPendingSellers = async (req, res) => {
+  try {
+    const pendingSellers = await User.find({
+      role: "seller",
+      isApproved: false
+    }).select("-password");
+
+    res.json({
+      success: true,
+      count: pendingSellers.length,
+      pendingSellers
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// APPROVE SELLER
+export const approveSeller = async (req, res) => {
+  try {
+    const seller = await User.findById(req.params.id);
+    if (!seller || seller.role !== "seller") {
+      return res.status(404).json({
+        success: false,
+        message: "Seller not found",
+      });
+    }
+
+    seller.isApproved = true;
+    await seller.save();
+
+    res.json({
+      success: true,
+      message: "Seller approved successfully",
+      seller
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
